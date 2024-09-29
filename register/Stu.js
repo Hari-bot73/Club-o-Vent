19f1cac5706cf49d74991182543d566ac6ec366e
Hari-bot73 <hareshhare685@gmail.com>
2024-09-29 21:39:43 +0530

    Complete

--- register/Stu.js
+++
@@ -1,6 +0,0 @@
-const signinbtn = document.getElementById('signinbtn');
-const container = document.querySelector('.container');
-
-signinbtn.addEventListener('click', () => {
-    container.classList.add('active');
-});
